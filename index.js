e6961ca2fb01fb4c9cb8954600f5684e7a51f474
DC_Paulius <DC_Paulius@bitbucket.org>
2022-07-25 12:51:30 +0300

    clean up

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
-// const express = require('express')
-// const path = require('path')
+const path = require('path')
 const PORT = process.env.PORT || 5000
 
-// express()
-//   .use(express.static(path.join(__dirname, 'public')))
-//   .set('views', path.join(__dirname, 'views'))
-//   .set('view engine', 'ejs')
-//   .get('/', (req, res) => res.render('pages/index'))
-//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-
-  const http = require('http');
-console.log('testjhgjhgjghj');
-// const hostname = '127.0.0.1';
-// const port = 5000;
+const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Worlddfds');
+  res.end('Hello');
 });
 
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
-
-server.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+server.listen(PORT, () => console.log(`Listening on ${PORT}`))
